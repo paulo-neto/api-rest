1044c62ac2ebcd29153e9c2703ef986edff23085
Paulo Neto <paneto@stefanini.com>
2017-08-25 16:20:04 -0300

    ajustando o context path

--- src/main/java/com/apirest/resources/config/ApplicationConf.java
+++ src/main/java/com/apirest/resources/config/ApplicationConf.java
@@ -3,7 +3,7 @@ package com.apirest.resources.config;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/api/")
+@ApplicationPath("api")
 public class ApplicationConf extends Application{
 
 }
